b45ac2afa84aecec7d21caa229ce5f0b1012d78f
Morpheus0x <42521763+Morpheus0x@users.noreply.github.com>
2018-09-22 16:29:40 +0200

    output with timestamp

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ MY_HOSTNAME=${MY_HOSTNAME:-""}
 GET_IP_SERVER=${GET_IP_SERVER:-"http://ipecho.net/plain"}
 MY_USER=${MY_USER:-""}
 MY_PASS=${MY_PASS:-""}
-WAIT_TIME=${WAIT_TIME:-"10"}
+WAIT_TIME=${WAIT_TIME:-"30"}
 
 while true; do
   EXT_IP=${EXT_IP:-$(curl -s $GET_IP_SERVER)}
-  echo "public IP: $EXT_IP"
-  curl -s 'https://api.dynu.com/nic/update?hostname='"${MY_HOSTNAME}"'&myip='"${EXT_IP}"'&username='"${MY_USER}"'&password='"${MY_PASS}"''
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Your public IP: $EXT_IP"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Dynu status: $(curl -s 'https://api.dynu.com/nic/update?hostname='"${MY_HOSTNAME}"'&myip='"${EXT_IP}"'&username='"${MY_USER}"'&password='"${MY_PASS}"'')"
   echo
   sleep $WAIT_TIME
 done
